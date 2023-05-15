634a89a3c4e7b9891730fa7031060faa872b419d
basementlivin <epedersen13@gmail.com>
2023-05-15 11:44:28 -0400

    Refactor SignupForm component

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,39 +1,34 @@
 import { useState } from "react";
 
 export default function SignupForm () {
-    
-    const SignUp = () => {
-
-        let [email, setEmail] = useState(""); 
-        
-        function handleEmailChange(e) {
-          setEmail(e.target.value);
-        }
-        
-
-        const handleSubmit = async (e) => {
-          // TODO
-        }
+      const [email, setEmail] = useState(""); 
       
-        return (
-          <form className="newsletter__signup" onSubmit={handleSubmit}>
-            <input
-              aria-label="enter your email"
-              name="email_address"
-              placeholder="enter your email"
-              required
-              type="text"
-              onChange={handleEmailChange}
-              value={email}
-            />
-            <button>SUBMIT</button>
-          </form>
-        )
+      const handleEmailChange = (e) => {
+        setEmail(e.target.value);
       }
-
-    return (
-        <>
-          <SignUp></SignUp>
-        </>
-    )
+      
+  
+      const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+          // TO-DO!
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    
+      return (
+        <form className="newsletter__signup" onSubmit={handleSubmit}>
+          <input
+            aria-label="enter your email"
+            name="email_address"
+            placeholder="enter your email"
+            required
+            type="text"
+            onChange={handleEmailChange}
+            value={email}
+          />
+          <button className="newsletter__submit">SUBMIT</button>
+        </form>
+      )
 }
