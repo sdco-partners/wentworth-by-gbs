614805b2cd94f6ac731873b15d94306210ca3e31
basementlivin <epedersen13@gmail.com>
2023-05-16 12:00:52 -0400

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import { ReactComponent as No162 } from "../assets/graphics/no162.svg";
+import { ReactComponent as Tagline } from "../assets/graphics/tagline.svg";
+import Mark from "../assets/graphics/mark.png";
+
+export default function Footer () {
+    return (
+        <>
+        <div className="footer">
+            <div className="footer__content">
+                <div className="footer__content__left">
+                    <img src={Mark} alt=""/>
+                </div>
+                <div className="footer__content__right">
+                    <No162></No162>
+                    <Tagline></Tagline>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import About from "../components/About";
+import Footer from "../components/Footer";
 
 export default function Landing () {
     return (
@@ -7,6 +8,7 @@ export default function Landing () {
         <div className="landing">
             <Header></Header>
             {/* <About></About> */}
+            <Footer></Footer>
         </div>
         </>
     )
