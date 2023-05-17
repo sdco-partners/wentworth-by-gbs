64ce31b19f7127d94dc32db607428e2916a6cdd5
basementlivin <epedersen13@gmail.com>
2023-05-17 16:31:38 -0400

    Implement custom Mailchimp form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,22 +1,42 @@
-import { useState } from "react";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 export default function SignupForm () {
-      const url = process.env.REACT_APP_MAILCHIMP_URL;
+  const url = process.env.REACT_APP_MAILCHIMP_URL;
 
-      return (
-        <MailchimpSubscribe url={url}/>
-        // <form className="newsletter__signup" onSubmit={handleSubmit}>
-        //   <input
-        //     aria-label="enter your email"
-        //     name="email_address"
-        //     placeholder="enter your email"
-        //     required
-        //     type="text"
-        //     onChange={handleEmailChange}
-        //     value={email}
-        //   />
-        //   <button className="newsletter__submit">SUBMIT</button>
-        // </form>
-      )
+  const CustomMailchimpForm = ({ status, message, onValidated }) => {
+    let email;
+    const submit = () =>
+      email &&
+      email.value.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email.value,
+      });
+      
+  return (
+    <>
+      {status === "sending" && <div className="subscribe-status">sending...</div>}
+      {status === "error" && (<div className="subscribe-status" dangerouslySetInnerHTML={{ __html: message }}/>)}
+      {status === "success" && (<div className="subscribe-status" dangerouslySetInnerHTML={{ __html: message }}/>)}
+      <input
+        ref={node => (email = node)}
+        type="email"
+        placeholder="enter your email"
+      />
+      <button onClick={submit}>Submit</button>
+    </>
+  );
+  };
+
+  return (
+      <MailchimpSubscribe
+        url={url}
+        render={({ subscribe, status, message }) => (
+          <CustomMailchimpForm
+            status={status}
+            message={message}
+            onValidated={formData => subscribe(formData)}
+          />
+        )}
+      />
+  );
 }
