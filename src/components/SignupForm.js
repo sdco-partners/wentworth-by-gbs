deef8a33750faa7b45bef551c648120364831d29
basementlivin <epedersen13@gmail.com>
2023-05-17 15:02:41 -0400

    Implement basic Mailchimp subscription form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,34 +1,22 @@
 import { useState } from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 export default function SignupForm () {
-      const [email, setEmail] = useState(""); 
-      
-      const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-      }
-      
-  
-      const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-          // TO-DO!
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    
+      const url = process.env.REACT_APP_MAILCHIMP_URL;
+
       return (
-        <form className="newsletter__signup" onSubmit={handleSubmit}>
-          <input
-            aria-label="enter your email"
-            name="email_address"
-            placeholder="enter your email"
-            required
-            type="text"
-            onChange={handleEmailChange}
-            value={email}
-          />
-          <button className="newsletter__submit">SUBMIT</button>
-        </form>
+        <MailchimpSubscribe url={url}/>
+        // <form className="newsletter__signup" onSubmit={handleSubmit}>
+        //   <input
+        //     aria-label="enter your email"
+        //     name="email_address"
+        //     placeholder="enter your email"
+        //     required
+        //     type="text"
+        //     onChange={handleEmailChange}
+        //     value={email}
+        //   />
+        //   <button className="newsletter__submit">SUBMIT</button>
+        // </form>
       )
 }
